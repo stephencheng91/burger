ed2c6f8a5c36de1293ab0b329dea02f3cd3d30ff
KuanToto <stephencheng91@gmail.com>
2019-06-04 18:57:46 -0700

    .gitignore

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Use public folder for styling and image
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//setup Handlebars.
+var exphbs = require("express-handlebars");
+
+//default web layout is using main handlebars
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+//Import route and the server access
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("Server listening on http://localhost"+PORT);
+});
